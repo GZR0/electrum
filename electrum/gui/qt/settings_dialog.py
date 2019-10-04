ee8a2dfae513974bbfcfece375cc5b52ab8ac539
MrNaif2018 <chuff184@gmail.com>
2019-10-04 21:08:10 +0300

    UI fix

--- electrum/gui/qt/settings_dialog.py
+++ electrum/gui/qt/settings_dialog.py
@@ -282,7 +282,7 @@ open. For this to work, your computer needs to be online regularly.""")
         # units
         units = base_units_list
         msg = (_('Base unit of your wallet.')
-               + '\n1 BTC = 1000 mBTC. 1 mBTC = 1000 bits. 1 bit = 100 sat.\n'
+               + '\n1 GZRO = 1000 mGZRO. 1 mGZRO = 1000 bits. 1 bit = 100 sat.\n'
                + _('This setting affects the Send tab, and all balance related fields.'))
         unit_label = HelpLabel(_('Base unit') + ':', msg)
         unit_combo = QComboBox()
